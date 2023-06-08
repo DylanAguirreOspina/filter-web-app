a50cc337ecc90341433d6b4fcf7e25285111bb83
DylanAguirreOspina <85912090+DylanAguirreOspina@users.noreply.github.com>
2023-06-08 12:36:27 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+function preload(){
+video=createCapture(VIDEO);
+video.size(300,300);
+video.hide();
+}
+
+function setup(){
+    canvas=createCanvas(300,300)
+        canvas.center()
+        video=createCapture(VIDEO);
+        video.size(300,300);
+        video.hide();
+        poseNet=ml5.poseNet(video, modelLoaded);
+        poseNet.on("pose", gotPoses);
+}
+
+function draw(){
+
+}
+
+function take_snapshot(){
+     save("myFilterimage.png");
+}
+
+function modelLoaded(){
+    console.log("PoseNet is Initialized");
+}
+
+function gotPoses(results){
+    console.log(results);
+    console.log("nosex= " +results[0].pose.nose.x);
+    console.log("nose y = "+ results[0].pose.nose.y);
+}
